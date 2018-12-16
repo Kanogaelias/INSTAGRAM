ad45dbc274b30e70b093aac989a9db5fd98cc153
Kanogaelias <kanogae@gmail.com>
2018-12-16 08:17:52 +0300

    add user list

--- instag/urls.py
+++ instag/urls.py
@@ -16,5 +16,5 @@ urlpatterns=[
     # url(r'^(?P<image_id>\d+)/preference/(?P<userpreference>\d+)/$', views.postpreference, name='postpreference'),
 ]
 
-# if settings.DEBUG:
-#     urlpatterns+= static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
