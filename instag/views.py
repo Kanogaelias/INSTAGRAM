d3097930d5f035b76d746ce7d9426d4c16822a9a
Kanogaelias <kanogae@gmail.com>
2018-12-16 08:26:32 +0300

    add a request method

--- instag/views.py
+++ instag/views.py
@@ -68,7 +68,16 @@ def image(request, id):
 
 
 
-   
+ if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            comment = form.cleaned_data['comment']
+
+            review = Review()
+            review.image = image
+            review.user = current_user
+            review.comment = comment
+            review.save()   
 
     else:
         form = ReviewForm()
