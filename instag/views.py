4949721c5c7c42c8967ca9eaf461b304ceca28b5
Kanogaelias <kanogae@gmail.com>
2018-12-16 08:23:51 +0300

    add newsletter and email imports

--- instag/views.py
+++ instag/views.py
@@ -5,8 +5,8 @@ from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 from .forms import NewImageForm, UpdatebioForm, ReviewForm
-# from .email import send_welcome_email
-# from .forms import NewsLetterForm
+from .email import send_welcome_email
+from .forms import NewsLetterForm
 
 # Views
 tags = tags.objects.all()
